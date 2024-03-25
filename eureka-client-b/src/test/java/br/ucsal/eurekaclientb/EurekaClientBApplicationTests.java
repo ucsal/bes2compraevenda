9923e8e460164b9815a132879dc0ed366e2e042d
gabrielsazevedo <gabriel.azeve04@gmail.com>
2024-03-25 15:30:58 -0300

    first commit

---
+++ eureka-client-b/src/test/java/br/ucsal/eurekaclientb/EurekaClientBApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ucsal.eurekaclientb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
